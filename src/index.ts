a6804618cb3d119e5c616bb94ab702d3eff67151
Jonathan Hays <jthays@lbl.gov>
2018-06-26 21:59:10 -0700

    Created basic html table display and added functionality and styling to the table.

--- src/index.ts
+++ src/index.ts
@@ -16,56 +16,73 @@ import '../style/index.css';
 /**
  * Initialization data for the jupyterlab_hpc extension.
  */
-const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_hpc',
-  autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterLab, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_hpc is activated!');
-    
+ const extension: JupyterLabPlugin<void> = {
+   id: 'jupyterlab_hpc',
+   autoStart: true,
+   requires: [ICommandPalette],
+   activate: (app: JupyterLab, palette: ICommandPalette) => {
+     console.log('JupyterLab extension jupyterlab_hpc is activated!');
+
 
     // Create a single widget
     let widget: Widget = new Widget();
     widget.id = 'nersc-hpc-jupyterlab';
     widget.title.label = 'SLURM Queue Monitor';
     widget.title.closable = true;
+    widget.addClass('jp-queueWidget');
 
     // Add a table element to the panel
     let queue_table = document.createElement('table');
+    // queue_table.classList.add('jp-queueTable', 'jp-qTableBorder');
+    queue_table.className = 'jp-queueTable';
     widget.node.appendChild(queue_table);
 
-    setInterval(populate_queue_table, 1000, queue_table);
+    populate_queue_table(queue_table);
+
+    // Refresh table at specified interval
+    // setInterval(populate_queue_table, 2000, queue_table);
 
     // Add an application command
     const command: string = 'hpc:open';
     app.commands.addCommand(command, {
-        label: 'SLURM Queue Monitor',
-        execute: () => {
-            if (!widget.isAttached) {
+      label: 'SLURM Queue Monitor',
+      execute: () => {
+        if (!widget.isAttached) {
                 // Attach the widget to the main work area if it's not there
                 app.shell.addToMainArea(widget);
-            }
+              }
             // Activate the widget
             app.shell.activateById(widget.id);
-        }
-    });
+          }
+        });
     // Add the command to the palette.
     palette.addItem({command, category: 'NERSC Tools'})
   }
 };
 
-
+// Pull queue data and populate html table
 function populate_queue_table(queue_table: HTMLElement) {
-    fetch('/shell/ps/aux').then(response => {
-        return response.text();
-    }).then(data => generate_table(data))
+  fetch('/shell/ps/aux').then(response => {
+    return response.text();
+  }).then(data => generate_table(queue_table, data))
 }
 
-function generate_table(data: string) {
-    let lines = data.split('\n');
-    for (var i = 0; i < lines.length; i++) {
-        console.log(lines[i].split(/[\s]+/))
-        console.log(lines[i].split(/[\s]+/).length)
+function generate_table(queue_table: HTMLElement, data: string) {
+  console.log("table refreshed!")
+  let lines = data.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    let row_values = lines[i].split(/[\s]+/)
+    let row = document.createElement('tr');
+    for (let j = 0; j < row_values.length; j++) {
+      let cell = document.createElement('td');
+      cell.className = 'jp-qTableBorder';
+      let cellText = document.createTextNode(row_values[j]);
+      // cell.style.fontSize = "12px";
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    queue_table.appendChild(row);
+  }
 }
 
-export default extension;
+  export default extension;
