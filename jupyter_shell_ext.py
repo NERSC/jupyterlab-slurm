390c519cd2ead8054e9701f25974a9098528736c
Jonathan Hays <jthays@lbl.gov>
2018-07-02 11:15:36 -0700

    Imported DataTables package, rendered default DataTables table from client-side generated HTML, began to use Ajax JSON DataTable generation implementation, downloaded DataTables images, fixed table styling and refresh bug, and other changes.

--- jupyter_shell_ext.py
+++ jupyter_shell_ext.py
@@ -1,4 +1,5 @@
 import subprocess
+import json
 
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
@@ -10,7 +11,27 @@ class ShellExecutionHandler(IPythonHandler):
         # finish(chunk) writes chunk to the output buffer and ends the HTTP request
         # Should probably process data here (on the server) and put it in a nice
         # format for loading into a table, instead of on the server!!!!
+        data = response.stdout.strip()
+
+        
+        # lines = data.split('\n')
+        # col_names = lines[0].split()
+        # data_dict = {}
+        # data_list = []
+
+        # for line in lines:
+        #     data_list += [[entry for entry in line.split()[:5]]]
+        # data_dict["data"] = data_list[:]
+
+        # for line in lines:
+        #     data_list += {col_names[i]: entry 
+        #     for i, entry in enumerate(line.split()[:5])}
+        # data_dict["data"] = data_list[:]
+
+        # print("GET called!")
         self.finish(response.stdout.strip())
+        # self.finish(json.dumps(data_list))
+        # self.finish(json.dumps(data_dict))
 
 def load_jupyter_server_extension(nb_server_app):
     """
@@ -19,7 +40,7 @@ def load_jupyter_server_extension(nb_server_app):
     Args:
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
-    print("extension loaded!")
+    print("server extension loaded!")
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
     route_pattern = url_path_join(web_app.settings['base_url'], '/shell/(.*)')
--- src/index.ts
+++ src/index.ts
@@ -21,35 +21,67 @@ import {
   Widget
 } from '@phosphor/widgets';
 
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-dt';
+import 'datatables.net-dt/css/jquery.dataTables.css';
 
 import '../style/index.css';
+
+
 
 class SlurmWidget extends Widget {
   /**
   * The table element containing SLURM queue data.
   */ 
   private queue_table: HTMLElement;
+  // private table: DataTables.Api;
+
+
 
   /**
   * Construct a new SLURM widget.
   */
   constructor() {
     super();
-
+    console.log("constructor called");
     this.id = 'nersc-hpc-jupyterlab';
     this.title.label = 'SLURM Queue Monitor';
     this.title.closable = true;
     this.addClass('jp-queueWidget');
 
     this.queue_table = document.createElement('table');
-    this.queue_table.className = 'jp-queueTable';
+    this.queue_table.setAttribute('id', 'queue');
+    // this.queue_table.className = 'jp-queueTable';
+    this.queue_table.classList.add('display', 'cell-border');
     this.node.appendChild(this.queue_table);
+
+
+    // TODO: add thead (and maybe tbody?) to queue_table! It
+    //       is required!!!
+
+    // $(document).ready(function() {
+    //   $('#queue').DataTable( {
+    //     ajax: {
+    //       url: "/shell/ps/aux",
+    //       dataSrc: "data"
+    //      },
+    //     columns: [
+    //     { data: 'name' },
+    //     { data: 'position' },
+    //     { data: 'salary' },
+    //     { data: 'state_date' },
+    //     { data: 'office' }
+    // ]
+    //   });
+    // });
 
   }
 
   public onUpdateRequest(msg: Message) {
     console.log("update request called!")
     this._update_queue_table();
+    // this._generate_queue_table();
   }
 
   // Pull queue data and populate queue_table
@@ -59,40 +91,88 @@ class SlurmWidget extends Widget {
     }).then(data => this._populate_queue_table(data));
   }
 
+  // public _generate_queue_table() {
+  //   var table = $('#queue').DataTable( {
+  //     ajax: {
+  //       url: "/shell/ps/aux",
+  //       dataSrc: "data"
+  //     },
+  //     columns: [
+  //     { data: 'name' },
+  //     { data: 'position' },
+  //     { data: 'salary' },
+  //     { data: 'state_date' },
+  //     { data: 'office' }
+  //     ]
+  //   });
+  //   table.ajax.reload();
+
+
+    // $(this.queue_table).ready(function() {
+    //   $('#queue').DataTable( {
+    //     ajax: {
+    //       url: "/shell/ps/aux",
+    //       dataSrc: "data"
+    //      }
+    //   });
+    // });
+  // }
+
+
+
   // Generate HTML table and populate with queue_data
   private _populate_queue_table(data: string) {
-    console.log("table refreshed!")
-
-
     // Clear table content
     while (this.queue_table.hasChildNodes()) {
       this.queue_table.removeChild(this.queue_table.lastChild);
     }
 
-    
-
-    
-    // TODO: fix this bad logic of clearing/not clearing the table...
-    // as of now the new data gets appended to the end of the existing 
-    // table! No good!
-    // this.queue_table = document.createElement('table');
+    // Adding classes again, necessary? The clearing of the
+    // table might be clearing the class list as well...
+    this.queue_table.classList.add('display', 'cell-border');
     let lines = data.split('\n');
-    // for (let i = 0; i < lines.length; i++) {
-    for (let i = 0; i < 10; i++) {      
+
+    // Create header and append to table
+    let header_row = document.createElement("tr");
+    let col_names = lines[0].split(/[\s]+/);
+    for (let i = 0; i < 8; i++) {
+      let headElem = document.createElement("th");
+      let headerText = document.createTextNode(col_names[i]);
+      headElem.appendChild(headerText);
+      header_row.appendChild(headElem);
+    }
+    let tblHead = document.createElement("thead");
+    tblHead.appendChild(header_row);
+    this.queue_table.appendChild(tblHead);
+
+    // Create table body and cells and append to table
+    let tblBody = document.createElement("tbody");
+    for (let i = 1; i < lines.length; i++) {
       let row_values = lines[i].split(/[\s]+/)
       let row = document.createElement('tr');
-      // for (let j = 0; j < row_values.length; j++) {
-      for (let j = 0; j < 5; j++) {
-
+      for (let j = 0; j < 8; j++) {
         let cell = document.createElement('td');
-        cell.className = 'jp-queueTable';
+        // cell.className = 'jp-queueTable';
+        // cell.className = 'jquery.dataTables';
+
         let cellText = document.createTextNode(row_values[j]);
         cell.appendChild(cellText);
         row.appendChild(cell);
       } // for (inner)
-      this.queue_table.appendChild(row);
+      tblBody.appendChild(row);
     } // for (outer)
+    this.queue_table.appendChild(tblBody);
+
+    $(document).ready(function() {
+      $('#queue').DataTable();
+    } );
+    
+
+
+
   } // _populate_queue_table
+
+
 } // class SlurmWidget
 
 
@@ -103,7 +183,7 @@ function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRe
   console.log('JupyterLab extension jupyterlab_hpc is activated!');
 
   // Declare a SLURM widget variable
-  let widget: SlurmWidget; //= new SlurmWidget();
+  let widget: SlurmWidget; 
 
   // Add an application command
   const command: string = 'hpc:open';
@@ -111,7 +191,7 @@ function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRe
     label: 'SLURM Queue Monitor',
     execute: () => {
       console.log("execute function entered!")
-      // So this function funs ONLY when the 'SLURM Queue Monitor' button
+      // So this function runs ONLY when the 'SLURM Queue Monitor' button
       // is pushed!
       if (!widget) {
         console.log("no widget region entered!")
@@ -132,6 +212,7 @@ function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRe
         // set interval here?
       } else {
         // Refresh the widget's state
+        console.log("else entered!")
         widget.update();
         // setInterval(() => widget.update(), 60000);
     }
