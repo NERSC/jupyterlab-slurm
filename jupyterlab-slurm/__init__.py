651d2ac3b1091d463e7e92a70f35e9898ff0802f
Jon Hays <mightyfancy@gmail.com>
2019-01-06 13:26:26 -0800

    Closes #6

--- jupyterlab-slurm/__init__.py
+++ jupyterlab-slurm/__init__.py
@@ -19,7 +19,7 @@ def load_jupyter_server_extension(nb_server_app):
     Args:
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
-    print('Server extension loaded!')
+    print('Jupyterlab-slurm server extension loaded!')
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
 
