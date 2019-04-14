08e79576ac7989e041e029fb4d0ab287853e12ca
Jon Hays <mightyfancy@gmail.com>
2019-04-13 19:14:35 -0700

    testing basic modal html

--- src/index.ts
+++ src/index.ts
@@ -256,7 +256,34 @@ class SlurmWidget extends Widget {
       alertContainer.setAttribute("id", "alertContainer");
       alertContainer.classList.add('container', 'alert-container');
       $('#jupyterlab-slurm').append(alertContainer);
-    });
+
+let modal = 
+`<!-- basic modal -->
+<div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">Basic Modal</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3>Modal Body</h3>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+let modalContainer = document.createElement('div');
+modalContainer.innerHTML = modal;
+$('#jupyterlab-slurm').append(modalContainer);
+
+    }); 
   }
 
   /**
