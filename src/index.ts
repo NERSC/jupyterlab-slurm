f6fc1a68a18f7c88bac9250adff9ce3d7c5a57e5
Jon Hays <mightyfancy@gmail.com>
2019-05-10 00:26:25 -0700

    revert to inline modal html string

--- src/index.ts
+++ src/index.ts
@@ -248,41 +248,41 @@ class SlurmWidget extends Widget {
 
 
 
-      // let modal = 
-      // `
-      // <div class="modal fade" id="submitJobModal" tabindex="-1" role="dialog" aria-labelledby="submitJobModalTitle" aria-hidden="true">
-      //   <div class="modal-dialog modal-dialog-centered" role="document">
-      //     <div class="modal-content">
-      //       <div class="modal-header">
-      //         <h3 class="modal-title" id="submitJobModalTitle">Submit a Batch Job</h3>
-      //         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-      //           <span aria-hidden="true">&times;</span>
-      //         </button>
-      //       </div>
-      //       <form id="jobSubmitForm" name="jobSubmit" role="form">
-      //         <div class="modal-body">
-      //           <div class="form-group">
-      //             <label for="path">Enter a file path containing a batch script</label>
-      //             <input type="text" name="path" id="batchPath" class="form-control">
-      //             <input type="submit" class="btn btn-primary" id="submitPath">
-      //           </div> 
-      //           <div class="form-group">
-      //             <label for="script">Enter a new batch script</label>
-      //             <textarea name="script" id="batchScript" rows="10" class="form-control"></textarea>
-      //             <input type="submit" class="btn btn-primary" id="submitScript">
-      //             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      //           </div>
-      //         </div>
-      //       </form>
-      //     </div>
-      //   </div>
-      // </div>
-      // `;
+      let modal = 
+      `
+      <div class="modal fade" id="submitJobModal" tabindex="-1" role="dialog" aria-labelledby="submitJobModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h3 class="modal-title" id="submitJobModalTitle">Submit a Batch Job</h3>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form id="jobSubmitForm" name="jobSubmit" role="form">
+              <div class="modal-body">
+                <div class="form-group">
+                  <label for="path">Enter a file path containing a batch script</label>
+                  <input type="text" name="path" id="batchPath" class="form-control">
+                  <input type="submit" class="btn btn-primary" id="submitPath">
+                </div> 
+                <div class="form-group">
+                  <label for="script">Enter a new batch script</label>
+                  <textarea name="script" id="batchScript" rows="10" class="form-control"></textarea>
+                  <input type="submit" class="btn btn-primary" id="submitScript">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      `;
 
       // TODO: import html string from external file for cleanliness
       // this commented out code doesn't work as of now
-      let modal = fs
-        .readFileSync("../templates/job_submit_modal.html", "utf-8");
+      // let modal = fs
+      //   .readFileSync("../templates/job_submit_modal.html", "utf-8");
 
       let modalContainer = document.createElement('div');
       modalContainer.innerHTML = modal;
