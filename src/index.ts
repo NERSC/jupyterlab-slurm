e8d8e263c384f1bb6c41c23f98152dd2288e1c6a
Jon Hays <mightyfancy@gmail.com>
2019-05-09 19:06:53 -0700

    Added columnRenderer function for truncating long output and displaying full output in tooltip

--- src/index.ts
+++ src/index.ts
@@ -158,7 +158,8 @@ class SlurmWidget extends Widget {
           {
             className: 'dt-center', 
             searchable: true,
-            targets: '_all'
+            targets: '_all',
+            render: self.columnRenderer()
           }
         ],
         // Set rowId to maintain selection after table reload 
@@ -462,6 +463,59 @@ class SlurmWidget extends Widget {
            this.reloadDataTable($('#queue').DataTable());
         }
       }
+    };
+  };
+
+ //  private esc(t: string) {
+ //    return t
+ //            .replace( /&/g, '&amp;' )
+ //            .replace( /</g, '&lt;' )
+ //            .replace( />/g, '&gt;' )
+ //            .replace( /"/g, '&quot;' );
+ // }
+
+  private columnRenderer() {
+    var esc = function ( t ) {
+        return t
+            .replace( /&/g, '&amp;' )
+            .replace( /</g, '&lt;' )
+            .replace( />/g, '&gt;' )
+            .replace( /"/g, '&quot;' );
+    };
+
+    return function ( d, type, row ) {
+        // Order, search and type get the original data
+        if ( type !== 'display' ) {
+            return d;
+        }
+ 
+        if ( typeof d !== 'number' && typeof d !== 'string' ) {
+            return d;
+        }
+ 
+        d = d.toString(); // cast numbers
+ 
+        if ( d.length < config["cutoff"] ) {
+            return d;
+        }
+ 
+        var shortened = d.substr(0, config["cutoff"]-1);
+ 
+        // Find the last white space character in the string
+        if ( config["wordbreak"] ) {
+            shortened = shortened.replace(/\s([^\s]*)$/, '');
+        }
+ 
+        // Protect against uncontrolled HTML input
+        if ( config["escapeHtml"] ) {
+            shortened = shortened
+                        .replace( /&/g, '&amp;' )
+                        .replace( /</g, '&lt;' )
+                        .replace( />/g, '&gt;' )
+                        .replace( /"/g, '&quot;' );
+        }
+ 
+        return '<span class="ellipsis" title="'+esc(d)+'">'+shortened+'&#8230;</span>';
     };
   };
 
