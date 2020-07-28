e33fd36e84611c396848343038e4628f43b1cdd6
Labanya Mukhopadhyay <labanyam@hotmail.com>
2020-07-28 09:10:54 -0700

    using slurm launcher

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -24,19 +24,25 @@ class TestAddfavs():
         self.driver.get("http://localhost:8845/lab")
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(20)
         
         #card_text = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[3]/div[2]/div/div/div[5]/div[2]/div")
         #assert card_text.text == "Slurm Queue"
         
-        slurm_card = self.driver.find_elements_by_class_name('jp-NerscLaunchIcon')
+        #slurm_card = self.driver.find_elements_by_class_name('jp-NerscLaunchIcon')
+        #slurm_card = self.driver.find_element_by_css_selector(".jp-NerscLaunchIcon.jp-Launcher-icon")
         #assert slurm_card.text == "Slurm Queue"
-        
-        self.driver.implicitly_wait(10)
-        self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        console = self.driver.find_element_by_css_selector("li.p-TabBar-tab:nth-child(3)")
         actions = ActionChains(self.driver)
-        actions.click(slurm_card[1]).perform()
+        actions.click(console).perform()
+        slurm_launcher = self.driver.find_element(By.XPATH, "//div[contains(text(),'Slurm Queue')]")
+        #slurm_launcher = self.driver.find_element_by_css_selector("li.p-CommandPalette-item:nth-child(31) > div:nth-child(2) > div:nth-child(1)")
+        self.driver.implicitly_wait(10)
+        self.driver.execute_script("return arguments[0].scrollIntoView(0, document.documentElement.scrollHeight-10);", slurm_launcher)   
         #actions.click(slurm_card[1]).perform()
+        #actions.click(slurm_card).perform()
+        actions = ActionChains(self.driver)
+        actions.click(slurm_launcher).perform()
         self.driver.implicitly_wait(10)
 
         # try: 
@@ -52,18 +58,18 @@ class TestAddfavs():
         # except NoSuchElementException:
         #     print('No slurm elements found')
 
-        jobid_text = self.driver.find_element(By.XPATH, "//h2[contains(text(),'HPC Tools')]")
-        assert jobid_text.text == "HPC Tools"
+        # jobid_text = self.driver.find_element(By.XPATH, "//h2[contains(text(),'HPC Tools')]")
+        # assert jobid_text.text == "HPC Tools"
 
-        # jobid_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'JOBID')]")
-        # assert jobid_text.text == "JOBID"
+        jobid_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'JOBID')]")
+        assert jobid_text.text == "JOBID"
 
-        # partition_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'PARTITION')]")
-        # assert partition_text.text == "PARTITION"
+        partition_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'PARTITION')]")
+        assert partition_text.text == "PARTITION"
 
-        # close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[2]/ul/li[2]/div[3]")
-        # actions = ActionChains(self.driver)
-        # actions.click(close_tab).perform()
+        close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[2]/ul/li[2]/div[3]")
+        actions = ActionChains(self.driver)
+        actions.click(close_tab).perform()
 
 if __name__ == '__main__':
   setup_method()
