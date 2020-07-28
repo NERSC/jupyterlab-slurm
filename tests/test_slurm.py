5ff44855e8ceb734b77bbb3e9864d4058a76caab
Labanya Mukhopadhyay <labanyam@hotmail.com>
2020-07-28 01:48:09 -0700

    add import exception

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementExceptionxs
 
 class TestAddfavs():
     def setup_method(self, method):
