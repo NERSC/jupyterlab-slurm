ef5fa4001e711b09c8f9424855b603b2257f2511
Labanya Mukhopadhyay <labanyam@hotmail.com>
2020-07-29 11:04:59 -0700

    travis trial 4

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -18,7 +18,7 @@ class TestAddfavs():
     def teardown_method(self, method):
         self.driver.quit()
   
-    def test_checktext(self):
+    def test_slurmcard(self):
         #import pdb; pdb.set_trace()
 
         self.driver.get("http://localhost:8845/lab")
@@ -32,13 +32,6 @@ class TestAddfavs():
         slurm_card = self.driver.find_elements_by_class_name('jp-NerscLaunchIcon')
         #slurm_card = self.driver.find_element_by_css_selector(".jp-NerscLaunchIcon.jp-Launcher-icon")
         #assert slurm_card.text == "Slurm Queue"
-        #console = self.driver.find_element_by_css_selector("li.p-TabBar-tab:nth-child(3)")
-        #console = self.driver.find_element(By.XPATH, '//*[@title="Commands (⇧ ⌘ C)"]')
-        
-        #self.driver.implicitly_wait(40)
-        #actions.click(console).perform()
-        #slurm_launcher = self.driver.find_element(By.XPATH, "//div[contains(text(),'Slurm Queue')]")
-        #slurm_launcher = self.driver.find_element_by_css_selector("li.p-CommandPalette-item:nth-child(31) > div:nth-child(2) > div:nth-child(1)")
         self.driver.implicitly_wait(10)
         #self.driver.execute_script("return arguments[0].scrollIntoView(0, document.documentElement.scrollHeight-10);", slurm_card)   
         #self.driver.implicitly_wait(20)
@@ -78,7 +71,60 @@ class TestAddfavs():
         actions = ActionChains(self.driver)
         actions.click(close_tab).perform()
 
+    def test_slurmcommands(self):
+        #import pdb; pdb.set_trace()
+
+        self.driver.get("http://localhost:8845/lab")
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+    
+        #console = self.driver.find_element_by_css_selector("li.p-TabBar-tab:nth-child(3)")
+        console = self.driver.find_element(By.XPATH, '//*[@title="Commands (⇧ ⌘ C)"]')
+        self.driver.implicitly_wait(40)
+        actions = ActionChains(self.driver)
+        actions.click(console).perform()
+        slurm_launcher = self.driver.find_element(By.XPATH, "//div[contains(text(),'Slurm Queue')]")
+        #slurm_launcher = self.driver.find_element_by_css_selector("li.p-CommandPalette-item:nth-child(31) > div:nth-child(2) > div:nth-child(1)")
+        self.driver.implicitly_wait(10)
+        self.driver.execute_script("return arguments[0].scrollIntoView(0, document.documentElement.scrollHeight-10);", slurm_launcher)   
+        
+        self.driver.implicitly_wait(10)
+        actions = ActionChains(self.driver)
+        
+        #actions.move_to_element(slurm_launcher).perform()
+        #actions = ActionChains(self.driver)
+        actions.click(slurm_launcher).perform()
+        self.driver.implicitly_wait(10)
+
+        # try: 
+        #     jobid_text = self.driver.find_element(By.XPATH, "//*[contains(text(),'JOBID')]")
+        #     assert jobid_text.text == "JOBID"
+
+        #     partition_text = self.driver.find_element(By.XPATH, "//*[contains(text(),'PARTITION')]")
+        #     assert partition_text.text == "PARTITION"
+
+        #     close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[2]/ul/li[2]/div[3]")
+        #     actions = ActionChains(self.driver)
+        #     actions.click(close_tab).perform()
+        # except NoSuchElementException:
+        #     print('No slurm elements found')
+
+        # jobid_text = self.driver.find_element(By.XPATH, "//h2[contains(text(),'HPC Tools')]")
+        # assert jobid_text.text == "HPC Tools"
+
+        jobid_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'JOBID')]")
+        assert jobid_text.text == "JOBID"
+
+        partition_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'PARTITION')]")
+        assert partition_text.text == "PARTITION"
+
+        close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[2]/ul/li[2]/div[3]")
+        actions = ActionChains(self.driver)
+        actions.click(close_tab).perform()
+
 if __name__ == '__main__':
   setup_method()
-  test_checktext()
+  test_slurmcard()
+  test_slurmcommands()
   teardown_method()
