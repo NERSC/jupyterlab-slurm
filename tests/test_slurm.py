3cbd1837857af538c4b002051211442e7c8ca420
Labanya Mukhopadhyay <labanyam@hotmail.com>
2020-07-30 22:02:24 -0700

    removing waits

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -81,54 +81,28 @@ class TestAddfavs():
         self.driver.implicitly_wait(40)
         self.driver.maximize_window()
         self.driver.implicitly_wait(40)
-    
-        #console = self.driver.find_element_by_css_selector("li.p-TabBar-tab:nth-child(3)")
         console = self.driver.find_element(By.XPATH, '//*[@data-id="command-palette"]')
-        self.driver.implicitly_wait(40)
+        #self.driver.implicitly_wait(40)
         actions = ActionChains(self.driver)
         actions.click(console).perform()
-        self.driver.implicitly_wait(40)
+        #self.driver.implicitly_wait(40)
         slurm_launcher = self.driver.find_element(By.XPATH, "//div[contains(text(),'Slurm Queue')]")
-        #slurm_launcher = self.driver.find_element_by_css_selector
-        #("li.p-CommandPalette-item:nth-child(31) > div:nth-child(2) > div:nth-child(1)")
-        self.driver.implicitly_wait(40)
+        #self.driver.implicitly_wait(40)
         self.driver.execute_script("return arguments[0].scrollIntoView(0, document.documentElement.scrollHeight-10);", slurm_launcher)
         actions = ActionChains(self.driver)
         #actions.move_to_element(slurm_launcher).perform()
-        self.driver.implicitly_wait(50)
+        #self.driver.implicitly_wait(50)
         actions.click(slurm_launcher).perform()
-        self.driver.implicitly_wait(40)
-
-        # try: 
-        #     jobid_text = self.driver.find_element(By.XPATH, "//*[contains(text(),'JOBID')]")
-        #     assert jobid_text.text == "JOBID"
-
-        #     partition_text = self.driver.find_element(By.XPATH, "//*[contains(text(),'PARTITION')]")
-        #     assert partition_text.text == "PARTITION"
-
-        #     close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[2]/ul/li[2]/div[3]")
-        #     actions = ActionChains(self.driver)
-        #     actions.click(close_tab).perform()
-        # except NoSuchElementException:
-        #     print('No slurm elements found')
-
-        # jobid_text = self.driver.find_element(By.XPATH, "//h2[contains(text(),'HPC Tools')]")
-        # assert jobid_text.text == "HPC Tools"
-
+        #self.driver.implicitly_wait(40)
         jobid_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'JOBID')]")
         assert jobid_text.text == "JOBID"
 
         partition_text = self.driver.find_element(By.XPATH, "//th[contains(text(),'PARTITION')]")
         assert partition_text.text == "PARTITION"
 
-        self.driver.implicitly_wait(40)
+        #self.driver.implicitly_wait(40)
 
-        #close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[2]/ul/li[2]/div[3]")
-        #close_tab = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[3]/div[3]/ul/li/div[3]")
-        #close_tab = self.driver.find_element(By.XPATH, '//*[@data-icon-id="233519a9-a23c-4415-8e4f-961d69ab1b08"]')
-        
-        #actions = ActionChains(self.driver)
-        #actions.click(close_tab).perform()
+
 
 if __name__ == '__main__':
   setup_method()
