b32671b643cf735495ce65d74954c31f914ee8d6
Matt Henderson <mhenderson@lbl.gov>
2021-06-01 09:43:46 -0700

    mostly working

--- jupyterlab_slurm/__init__.py
+++ jupyterlab_slurm/__init__.py
@@ -1,60 +1,40 @@
 """JupyterLab Slurm : view and manage Slurm queue from JupyterLab """
+import json
+import logging
+from pathlib import Path
 
-from .slurm import ScancelHandler
-from .slurm import ScontrolHandler
-from .slurm import SbatchHandler
-from .slurm import SqueueHandler
-from .slurm import UserFetchHandler
+from .handlers import setup_handlers
 
-from notebook.utils import url_path_join
-from os import path
+HERE = Path(__file__).parent.resolve()
 
-__version__ = "2.0.0"
-
-
-def _jupyter_server_extension_paths():
-    return [{
-        "module": "jupyterlab_slurm"
-    }]
+with (HERE / "labextension" / "package.json").open() as fid:
+    data = json.load(fid)
 
 
-def load_jupyter_server_extension(nb_server_app):
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": data["name"]}]
+
+
+def _jupyter_server_extension_points():
+    return [{"module": "jupyterlab_slurm"}]
+
+
+def _load_jupyter_server_extension(server_app):
     """
     Called when the extension is loaded.
 
     Args:
-        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+        server_app (): handle to the JupyterLab webserver instance.
     """
-    print('jupyterlab_slurm server extension loaded')
-    web_app = nb_server_app.web_app
-    host_pattern = '.*$'
-
-    base_url = web_app.settings['base_url']
-
+    server_app.log.info('jupyterlab_slurm server extension loaded')
+    web_app = server_app.web_app
     temporary_directory = web_app.settings['temporary_directory'] if 'temporary_directory' in web_app.settings else None
+    server_app.log.addHandler(logging.FileHandler('/tmp/jupyter_debug'))
+    server_app.log.setLevel(logging.DEBUG)
 
-    spath = path.normpath(
-        web_app.settings['spath']) + "/" if 'spath' in web_app.settings else ''
+    # add get_example url
+    setup_handlers(web_app, temporary_directory=temporary_directory, log=server_app.log)
 
-    def obtain_path(method):
-        return web_app.settings[method + '_path'] if method + '_path' in web_app.settings else spath + method
 
-    squeue_path = obtain_path("squeue")
-    scancel_path = obtain_path("scancel")
-    scontrol_path = obtain_path("scontrol")
-    sbatch_path = obtain_path("sbatch")
-
-    print("Starting up....", squeue_path, scancel_path,
-          scontrol_path, sbatch_path, sep='\n')
-
-    web_app.add_handlers(host_pattern, [
-        (url_path_join(base_url, '/squeue'),
-         SqueueHandler, dict(squeue=squeue_path)),
-        (url_path_join(base_url, '/scancel'),
-         ScancelHandler, dict(scancel=scancel_path)),
-        (url_path_join(base_url, '/scontrol/(?P<command>.*)'),
-         ScontrolHandler, dict(scontrol=scontrol_path)),
-        (url_path_join(base_url, '/sbatch'),
-         SbatchHandler, dict(sbatch=sbatch_path, temporary_directory=temporary_directory)),
-        (url_path_join(base_url, '/user'), UserFetchHandler)
-    ])
+# classic notebook backward compatibility
+load_jupyter_server_extension = _load_jupyter_server_extension
--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, ReactNode } from 'react';
 import { Pagination } from 'react-bootstrap';
 
 namespace types {
   export type Props = {
     numPages: number;
-    onChange?: (number) => void;
+    onChange?: (numPages: number) => void;
   };
 
   export type State = {
@@ -16,18 +16,18 @@ export default class Pager extends Component<types.Props, types.State> {
   constructor(props: types.Props) {
     super(props);
     this.state = {
-      currentPage: 1,
+      currentPage: 1
     };
   }
 
-  handlePageChange(page: number) {
+  handlePageChange(page: number): void {
     this.setState({ currentPage: page });
     if (this.props.onChange) {
       this.props.onChange(page);
     }
   }
 
-  render() {
+  render(): ReactNode {
     const { numPages } = this.props;
     const { currentPage } = this.state;
     const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
@@ -36,23 +36,49 @@ export default class Pager extends Component<types.Props, types.State> {
       startPage + 1,
       startPage + 2,
       startPage + 3,
-      startPage + 4,
+      startPage + 4
     ].filter(page => page >= 1 && page <= numPages);
     const showFirstPageShortcut = range.indexOf(1) < 0;
     const showLastPageShortcut = range.indexOf(numPages) < 0;
     return (
       <Pagination>
-        {numPages > 1 && <Pagination.Prev onClick={this.handlePageChange.bind(this, currentPage - 1)}/>}
-        {showFirstPageShortcut && <Pagination.Item onClick={this.handlePageChange.bind(this, 1)}>{1}</Pagination.Item>}
+        {numPages > 1 && (
+          <Pagination.Prev
+            onClick={this.handlePageChange.bind(this, currentPage - 1)}
+          />
+        )}
+        {showFirstPageShortcut && (
+          <Pagination.Item onClick={this.handlePageChange.bind(this, 1)}>
+            {1}
+          </Pagination.Item>
+        )}
         {showFirstPageShortcut && <Pagination.Ellipsis />}
-        {range.map((page, index) => page == currentPage
-          ? <Pagination.Item active key={`page-${index}`}>{currentPage}</Pagination.Item>
-          : <Pagination.Item onClick={this.handlePageChange.bind(this, page)} key={`page-${index}`}>{page}</Pagination.Item>
+        {range.map((page, index) =>
+          page === currentPage ? (
+            <Pagination.Item active key={`page-${index}`}>
+              {currentPage}
+            </Pagination.Item>
+          ) : (
+            <Pagination.Item
+              onClick={this.handlePageChange.bind(this, page)}
+              key={`page-${index}`}
+            >
+              {page}
+            </Pagination.Item>
+          )
         )}
         {showLastPageShortcut && <Pagination.Ellipsis />}
-        {showLastPageShortcut && <Pagination.Item onClick={this.handlePageChange.bind(this, numPages)}>{numPages}</Pagination.Item>}
-        {numPages > 1 && <Pagination.Next onClick={this.handlePageChange.bind(this, currentPage + 1)} />}
+        {showLastPageShortcut && (
+          <Pagination.Item onClick={this.handlePageChange.bind(this, numPages)}>
+            {numPages}
+          </Pagination.Item>
+        )}
+        {numPages > 1 && (
+          <Pagination.Next
+            onClick={this.handlePageChange.bind(this, currentPage + 1)}
+          />
+        )}
       </Pagination>
-    )
+    );
   }
 }
